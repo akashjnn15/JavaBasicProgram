c7d6f169733d3e3bd0599108ecd47be796a45637
akashjnn15 <akashjnn15@gmail.com>
2022-10-26 19:43:37 +0530

    added withoutSemicolon

---
+++ src/WithoutSemiColon.java
@@ -0,0 +1,16 @@
+
+
+public class WithoutSemiColon {
+	
+	public static void main(String[] args) {
+		if(System.out.append("Hello World"+ "\n")==null) {
+			
+		}
+		
+		if(System.out.printf("Hello World")==null) {
+			
+		}
+			
+	}
+
+}
